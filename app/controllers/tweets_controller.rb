4b6ef300ad7479fc0653492d93f99dd207ffac90
asuf29 <asuff299@gmail.com>
2024-12-10 11:49:32 +0300

    add tweet controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,40 @@
+class TweetsController < ApplicationController
+  #tweetleri listeler
+  def index
+    @tweets = Tweet.all
+  end
+  #tweet detay sayfası
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  #yeni tweet oluşturma sayfası
+  def new
+    @tweet = Tweet.new 
+  end
+  #yeni tweet oluşturma işlemi
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path, notice: "Tweet başarıyla oluşturuldu."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  #tweet düzenleme sayfası
+  def edit
+
+  end
+  #tweet güncelleme işlemi
+  def update
+
+  end
+  #tweet silme işlemi
+  def destroy
+
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body, :user_id)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   #bir kullanıcının birden fazla tweeti olabilir
   has_many :tweets
   #email ve username alanlarının boş olmamasını ve benzersiz olması gerektiğini belirttik.
-  validates :email, :username, presence: true, uniquenness: true 
+  validates :email, :username, presence: true, uniqueness: true 
 
   #first_name ve last_name alanlarının boş olmaması gerektiğini belirttik.
   validates :first_name, :last_name, presence: true
