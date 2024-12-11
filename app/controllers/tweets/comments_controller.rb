553c8ffa35d427583d6c3a87c5383c5af238dcf9
asuf29 <asuff299@gmail.com>
2024-12-12 00:13:17 +0300

    comment create operations

--- app/controllers/tweets/comments_controller.rb
+++ app/controllers/tweets/comments_controller.rb
@@ -1,6 +1,16 @@
 class Tweets::CommentsController < ApplicationController
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    comment = @tweet.comments.new(comment_params)
+    if comment.save 
+      redirect_to tweet_path(@tweet)
+    end
   end
   def update
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :user_id)
   end
 end
