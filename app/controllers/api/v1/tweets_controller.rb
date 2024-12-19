565c9b25806f5ee8a25ccdb9c6b4a3ade1c7a920
asuf29 <asuff299@gmail.com>
2024-12-19 14:03:39 +0300

    add resources tweets

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -4,9 +4,12 @@ class Api::V1::TweetsController < ApplicationController
   def index
     user = User.find(params[:user_id])
     tweets = user.tweets
-
-    render json: tweets
-  rescue ActiveRecord: :RecordNotFound
-    render json: {error: "User not found"}, status: :not_found
+  
+    render json: {
+      user_id: user.id,
+      tweets: tweets.as_json(only: [:id, :body, :created_at, :updated_at])
+    } and return
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :tweets
     end
   end
 end
