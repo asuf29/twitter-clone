462f106fa3fca61b2714d4774eba4aaa5f861fc8
asuf29 <asuff299@gmail.com>
2024-12-19 14:07:20 +0300

    I wrote an endpoint that takes the tweet id and returns the tweet content and comments

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -12,4 +12,27 @@ class Api::V1::TweetsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render json: { error: "User not found" }, status: :not_found
   end
+
+  def show
+    tweet = Tweet.includes(:comments).find(params[:id])
+
+    render json: {
+      tweet: {
+        id: tweet.id,
+        body: tweet.body,
+        created_at: tweet.created_at,
+        updated_at: tweet.updated_at
+      },
+      comments: tweet.comments.map do |comment| 
+        {
+          id: comment.id,
+          body: comment.body,
+          created_at: comment.created_at,
+          updated_at: comment.updated_at
+        }
+      end
+    }
+  rescue
+    render json: {error: "Tweet not found"}, status: :not_found
+  end
 end
